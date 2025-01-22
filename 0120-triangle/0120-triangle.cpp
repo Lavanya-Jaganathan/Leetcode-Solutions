725d90bce00c88fa2f83e8c04c7b440eb6052ba8
Lavanya-Jaganathan <lavanyaj2411@gmail.com>
2025-01-22 11:41:38 +0530

    Time: 1 ms (21.8%), Space: 13.3 MB (9.92%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -18,9 +18,9 @@ int solve(int i,int j,vector<vector<int>>&tri,vector<vector<int>>&dp)
 }
     int minimumTotal(vector<vector<int>>& tri)
     {
-        int n=tri.size();
-        int m=tri[0].size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int m=tri.size();
+        int n=tri[0].size();
+        vector<vector<int>>dp(m,vector<int>(m,-1));
         return solve(0,0,tri,dp);
     }
 };
